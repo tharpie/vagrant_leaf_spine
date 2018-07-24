5ff6583748de464d21b0f56bcddb50909f86e7bf
tharpie <tharp.matthew@gmail.com>
2018-07-24 08:28:35 -0500

    update to host routes

--- scripts/host-config.sh
+++ scripts/host-config.sh
@@ -14,6 +14,11 @@ ROUTES="
 192.168.11.0/24 via $BASE.1 dev eth1
 192.168.12.0/24 via $BASE.1 dev eth1
 192.168.13.0/24 via $BASE.1 dev eth1
+1.1.1.1/32 via $BASE.1 dev eth1
+2.2.2.2/32 via $BASE.1 dev eth1
+3.3.3.3/32 via $BASE.1 dev eth1
+101.101.101.101/32 via $BASE.1 dev eth1
+202.202.202.202/32 via $BASE.1 dev eth1
 "
 
 sudo echo "$IFCFG" > /etc/sysconfig/network-scripts/ifcfg-eth1
